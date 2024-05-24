3a79322eccff130a637ceed163846c8cd65170e8
xiaoyukou <xiaoyukou@microsoft.com>
2024-05-24 15:17:07 +0800

    k

--- scripts/inference.py
+++ scripts/inference.py
@@ -22,10 +22,12 @@ import random
 import sys
 
 import datasets
+from datasets import load_dataset, load_metric
 import torch
 import transformers
 from transformers import AutoModelForCausalLM, set_seed
 import json
+from tqdm import tqdm
 
 from alignment import (
     DataArguments,
@@ -43,6 +45,7 @@ from alignment import (
 )
 from trl import SFTTrainer, setup_chat_format
 
+from transformers import AutoTokenizer, pipeline
 
 logger = logging.getLogger(__name__)
 
@@ -78,28 +81,6 @@ def main():
     logger.info(f"Data parameters {data_args}")
     logger.info(f"Training/evaluation parameters {training_args}")
 
-    ###############
-    # Load datasets
-    ###############
-    raw_datasets = get_datasets(
-        data_args,
-        splits=data_args.dataset_splits,
-        configs=data_args.dataset_configs,
-        columns_to_keep=["messages", "chosen", "rejected", "prompt", "completion", "label"],
-    )
-    logger.info(
-        f"Training on the following datasets and their proportions: {[split + ' : ' + str(dset.num_rows) for split, dset in raw_datasets.items()]}"
-    )
-    column_names = list(raw_datasets["train"].features)
-
-    # remove this when done debugging
-    from datasets import DatasetDict
-    indices = range(0, 100)
-
-    dataset_dict = {"train": raw_datasets["train"].select(indices),
-                    "test": raw_datasets["test"].select(indices)}
-    raw_datasets = DatasetDict(dataset_dict)
-
     ################
     # Load tokenizer
     ################
@@ -123,132 +104,43 @@ def main():
         device_map=get_kbit_device_map() if quantization_config is not None else None,
         quantization_config=quantization_config,
     )
+    model = AutoModelForCausalLM.from_pretrained(model_args.model_name_or_path, **model_kwargs)
+    model, tokenizer = setup_chat_format(model, tokenizer)
 
-    model = model_args.model_name_or_path
-    # For ChatML we need to add special tokens and resize the embedding layer
-    if "<|im_start|>" in tokenizer.chat_template and "gemma-tokenizer-chatml" not in tokenizer.name_or_path:
-        model = AutoModelForCausalLM.from_pretrained(model_args.model_name_or_path, **model_kwargs)
-        model, tokenizer = setup_chat_format(model, tokenizer)
-        model_kwargs = None
+    # load into pipeline
+    pipe = pipeline("text-generation", model=model, tokenizer=tokenizer)
 
-    #####################
-    # Apply chat template
-    #####################
-    raw_datasets = raw_datasets.map(
-        apply_chat_template,
-        fn_kwargs={
-            "tokenizer": tokenizer,
-            "task": "sft",
-            "auto_insert_empty_system_msg": data_args.auto_insert_empty_system_msg,
-        },
-        num_proc=data_args.preprocessing_num_workers,
-        remove_columns=column_names,
-        desc="Applying chat template",
-    )
-
-    ##########################
-    # Decontaminate benchmarks
-    ##########################
-    num_raw_train_samples = len(raw_datasets["train"])
-    raw_datasets = raw_datasets.filter(decontaminate_humaneval, batched=True, batch_size=10_000, num_proc=1)
-    num_filtered_train_samples = num_raw_train_samples - len(raw_datasets["train"])
-    logger.info(
-        f"Decontaminated {num_filtered_train_samples} ({num_filtered_train_samples/num_raw_train_samples * 100:.2f}%) samples from the training set."
-    )
-
-    train_dataset = raw_datasets["train"]
-    eval_dataset = raw_datasets["test"]
+    ###############
+    # Load datasets
+    ###############
+    eval_dataset = load_dataset('json', data_files=data_args.dataset_mixer)
 
     with training_args.main_process_first(desc="Log a few random samples from the processed training set"):
-        for index in random.sample(range(len(raw_datasets["train"])), 3):
-            logger.info(f"Sample {index} of the processed training set:\n\n{raw_datasets['train'][index]['text']}")
-
-    ########################
-    # Initialize the Trainer
-    ########################
-    trainer = SFTTrainer(
-        model=model,
-        model_init_kwargs=model_kwargs,
-        args=training_args,
-        train_dataset=train_dataset,
-        eval_dataset=eval_dataset,
-        dataset_text_field="text",
-        max_seq_length=training_args.max_seq_length,
-        tokenizer=tokenizer,
-        packing=True,
-        peft_config=get_peft_config(model_args),
-        dataset_kwargs=training_args.dataset_kwargs,
-    )
-    '''
-    ###############
-    # Training loop
-    ###############
-    logger.info("*** Train ***")
-    checkpoint = None
-    if training_args.resume_from_checkpoint is not None:
-        checkpoint = training_args.resume_from_checkpoint
-    elif last_checkpoint is not None:
-        checkpoint = last_checkpoint
-    train_result = trainer.train(resume_from_checkpoint=checkpoint)
-    metrics = train_result.metrics
-    metrics["train_samples"] = len(train_dataset)
-    trainer.log_metrics("train", metrics)
-    trainer.save_metrics("train", metrics)
-    trainer.save_state()
-
-    ##################################
-    # Save model and create model card
-    ##################################
-    logger.info("*** Save model ***")
-    trainer.save_model(training_args.output_dir)
-    logger.info(f"Model saved to {training_args.output_dir}")
-
-    # Save everything else on main process
-    kwargs = {
-        "finetuned_from": model_args.model_name_or_path,
-        "dataset": list(data_args.dataset_mixer.keys()),
-        "dataset_tags": list(data_args.dataset_mixer.keys()),
-        "tags": ["alignment-handbook"],
-    }
-    if trainer.accelerator.is_main_process:
-        trainer.create_model_card(**kwargs)
-        # Restore k,v cache for fast inference
-        trainer.model.config.use_cache = True
-        trainer.model.config.save_pretrained(training_args.output_dir)
-    '''
-    ##########
-    # Evaluate
-    ##########
-    if training_args.do_eval:
-        logger.info("*** Evaluate ***")
-        metrics = trainer.evaluate()
-        metrics["eval_samples"] = len(eval_dataset)
-        trainer.log_metrics("eval", metrics)
-        trainer.save_metrics("eval", metrics)
+        for index in random.sample(range(len(eval_dataset)), 3):
+            logger.info(f"Sample {index} of the processed evaluate set:\n\n{eval_dataset[index]['prompt']}")
 
     ##########
     # Inference and Save Responses
     ##########
     logger.info("*** Inference and Save Responses ***")
     responses = []
-    model.eval()
-    for example in eval_dataset:
-        inputs = tokenizer(example["prompt"], return_tensors="pt").to(training_args.device)
-        with torch.no_grad():
-            outputs = model.generate(**inputs, max_length=training_args.generation_max_length)
-        response = tokenizer.decode(outputs[0], skip_special_tokens=True)
-        responses.append({"prompt": example["prompt"], "response": response})
+    '''
+    
+    generator = pipeline(model="HuggingFaceH4/zephyr-7b-beta")
+    # Zephyr-beta is a conversational model, so let's pass it a chat instead of a single string
+    generator([{"role": "user", "content": "What is the capital of France? Answer in one word."}], do_sample=False, max_new_tokens=2)
+    
+    pipe = pipeline("text-generation", "HuggingFaceH4/zephyr-7b-beta")
+    messages = [
+        {
+            "role": "system",
+            "content": "You are a friendly chatbot who always responds in the style of a pirate",
+        },
+        {"role": "user", "content": "How many helicopters can a human eat in one sitting?"},
+    ]
+    print(pipe(messages, max_new_tokens=128)[0]['generated_text'][-1])  # Print the assistant's response
+    '''
 
-    output_file = f"{training_args.output_dir}/responses.json"
-    with open(output_file, "w", encoding="utf-8") as f:
-        json.dump(responses, f, ensure_ascii=False, indent=4)
-    logger.info(f"Responses saved to {output_file}")
-
-    if training_args.push_to_hub is True:
-        logger.info("Pushing to hub...")
-        trainer.push_to_hub(**kwargs)
-
-    logger.info("*** Training complete ***")
 
 
 if __name__ == "__main__":
