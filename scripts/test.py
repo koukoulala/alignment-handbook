2ad5b97385ceab048036267432eb8e36a4632aa6
xiaoyukou <xiaoyukou@microsoft.com>
2024-05-25 19:19:58 +0800

    k

--- scripts/test.py
+++ scripts/test.py
@@ -1,8 +1,28 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+
+# specify how to quantize the model
+quantization_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype="torch.float16",
+)
 
 checkpoint = "../ckpts/Mistral-7B-Instruct-v0.2/"
+model = AutoModelForCausalLM.from_pretrained(checkpoint, quantization_config=True, device_map="auto")
 tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-model = AutoModelForCausalLM.from_pretrained(checkpoint)  # You may want to use bfloat16 and/or move to GPU here
+
+messages = [
+    {"role": "user", "content": "What is your favourite condiment?"},
+    {"role": "assistant",
+     "content": "Well, I'm quite partial to a good squeeze of fresh lemon juice. It adds just the right amount of zesty flavour to whatever I'm cooking up in the kitchen!"},
+    {"role": "user", "content": "Do you have mayonnaise recipes?"}
+]
+
+model_inputs = tokenizer.apply_chat_template(messages, return_tensors="pt").to("cuda")
+
+generated_ids = model.generate(model_inputs, max_new_tokens=100, do_sample=True)
+print(tokenizer.batch_decode(generated_ids)[0])
 
 '''
 messages = [
@@ -10,15 +30,3 @@ messages = [
      "content": "Please generate 4 Ad Headline in English language, based on the following information:\nFinalUrl: https://ripack.com/en/shrinking-gun/?mtm_campaign=Notoriete_EN&mtm_source=BingAds&mtm_medium=ppc \nDomain: ripack.com \nCategory: Retail -- Home & Garden \nLandingPage:  . Extensions for Ripack heat shrink tools | Leader for professional packaging | Ripack;  . Accueil . Heat Shrink Gun . Assistance . Distributor . Register a product . Ask for a quote . Products . Heat Shrink Guns . Ripack 3000 + . Ripack 3000 . Ripack 2500 . Ripack 2100 . Film sealing machines . Multicover 935 film dispenser . Multicover 955 . Multicover 960 . Gas bottle trolleys . Automated flam \nCharacterLimit: between 10 to 20 characters. \n"},
  ]
 '''
-messages = [
-    {
-        "role": "system",
-        "content": "You are a friendly chatbot who always responds in the style of a pirate",
-    },
-    {"role": "user", "content": "How many helicopters can a human eat in one sitting?"},
- ]
-tokenized_chat = tokenizer.apply_chat_template(messages, tokenize=True, add_generation_prompt=True, return_tensors="pt")
-print(tokenizer.decode(tokenized_chat[0]))
-
-outputs = model.generate(tokenized_chat, max_new_tokens=128)
-print(tokenizer.decode(outputs[0]))
