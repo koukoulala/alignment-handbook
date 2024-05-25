10d9fc4666398935934a2f07d6686e70f7bb4dae
xiaoyukou <xiaoyukou@microsoft.com>
2024-05-25 20:40:49 +0800

    k

--- scripts/test.py
+++ scripts/test.py
@@ -9,13 +9,14 @@ quantization_config = BitsAndBytesConfig(
     bnb_4bit_compute_dtype="float16",
 )
 
-checkpoint = "./ckpts/Mistral-7B-Instruct-v0.2/"
+#checkpoint = "./ckpts/Mistral-7B-Instruct-v0.2/"
+checkpoint = "./output/asset-generation-sft-qlora/checkpoint-4000/"
 model = AutoModelForCausalLM.from_pretrained(checkpoint, quantization_config=quantization_config, device_map="auto")
 tokenizer = AutoTokenizer.from_pretrained(checkpoint)
 
 lora_ckpt = "./output/asset-generation-sft-qlora/checkpoint-4000/"
-model = PeftModel.from_pretrained(model, lora_ckpt, adapter_name="asset_generation")
-model.set_adapter("asset_generation")
+#model = PeftModel.from_pretrained(model, lora_ckpt, adapter_name="asset_generation")
+#model.set_adapter("asset_generation")
 messages = [
     {"role": "user",
      "content": "Please generate 4 Ad Headline in English language, based on the following information:\nFinalUrl: https://ripack.com/en/shrinking-gun/?mtm_campaign=Notoriete_EN&mtm_source=BingAds&mtm_medium=ppc \nDomain: ripack.com \nCategory: Retail -- Home & Garden \nLandingPage:  . Extensions for Ripack heat shrink tools | Leader for professional packaging | Ripack;  . Accueil . Heat Shrink Gun . Assistance . Distributor . Register a product . Ask for a quote . Products . Heat Shrink Guns . Ripack 3000 + . Ripack 3000 . Ripack 2500 . Ripack 2100 . Film sealing machines . Multicover 935 film dispenser . Multicover 955 . Multicover 960 . Gas bottle trolleys . Automated flam \nCharacterLimit: between 10 to 20 characters. \n"},
