09b079458bfcc2eab1c7cc258134cbb207273593
xiaoyukou <xiaoyukou@microsoft.com>
2024-05-25 19:40:36 +0800

    k

--- scripts/test.py
+++ scripts/test.py
@@ -11,22 +11,22 @@ quantization_config = BitsAndBytesConfig(
 checkpoint = "../ckpts/Mistral-7B-Instruct-v0.2/"
 model = AutoModelForCausalLM.from_pretrained(checkpoint, quantization_config=quantization_config, device_map="auto")
 tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-
+messages = [
+    {"role": "user",
+     "content": "Please generate 4 Ad Headline in English language, based on the following information:\nFinalUrl: https://ripack.com/en/shrinking-gun/?mtm_campaign=Notoriete_EN&mtm_source=BingAds&mtm_medium=ppc \nDomain: ripack.com \nCategory: Retail -- Home & Garden \nLandingPage:  . Extensions for Ripack heat shrink tools | Leader for professional packaging | Ripack;  . Accueil . Heat Shrink Gun . Assistance . Distributor . Register a product . Ask for a quote . Products . Heat Shrink Guns . Ripack 3000 + . Ripack 3000 . Ripack 2500 . Ripack 2100 . Film sealing machines . Multicover 935 film dispenser . Multicover 955 . Multicover 960 . Gas bottle trolleys . Automated flam \nCharacterLimit: between 10 to 20 characters. \n"},
+]
+'''
 messages = [
     {"role": "user", "content": "What is your favourite condiment?"},
     {"role": "assistant",
      "content": "Well, I'm quite partial to a good squeeze of fresh lemon juice. It adds just the right amount of zesty flavour to whatever I'm cooking up in the kitchen!"},
     {"role": "user", "content": "Do you have mayonnaise recipes?"}
 ]
-
+'''
 model_inputs = tokenizer.apply_chat_template(messages, return_tensors="pt").to("cuda")
 
 generated_ids = model.generate(model_inputs, max_new_tokens=100, do_sample=True)
 print(tokenizer.batch_decode(generated_ids)[0])
 
-'''
-messages = [
-    {"role": "user",
-     "content": "Please generate 4 Ad Headline in English language, based on the following information:\nFinalUrl: https://ripack.com/en/shrinking-gun/?mtm_campaign=Notoriete_EN&mtm_source=BingAds&mtm_medium=ppc \nDomain: ripack.com \nCategory: Retail -- Home & Garden \nLandingPage:  . Extensions for Ripack heat shrink tools | Leader for professional packaging | Ripack;  . Accueil . Heat Shrink Gun . Assistance . Distributor . Register a product . Ask for a quote . Products . Heat Shrink Guns . Ripack 3000 + . Ripack 3000 . Ripack 2500 . Ripack 2100 . Film sealing machines . Multicover 935 film dispenser . Multicover 955 . Multicover 960 . Gas bottle trolleys . Automated flam \nCharacterLimit: between 10 to 20 characters. \n"},
- ]
-'''
+
+
